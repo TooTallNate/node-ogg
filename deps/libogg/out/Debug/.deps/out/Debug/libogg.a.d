4f27b09cc310248e1278277d0393378139a13b9e
Nathan Rajlich <nathan@tootallnate.net>
2012-11-08 23:45:27 -0800

    libogg: remove generated gyp files

--- deps/libogg/out/Debug/.deps/out/Debug/libogg.a.d
+++
@@ -1,1 +0,0 @@
-cmd_out/Debug/libogg.a := rm -f out/Debug/libogg.a && ./gyp-mac-tool filter-libtool libtool -static -o out/Debug/libogg.a out/Debug/obj.target/ogg/src/framing.o out/Debug/obj.target/ogg/src/bitwise.o
--- deps/libogg/out/Debug/.deps/out/Debug/obj.target/ogg/src/framing.o.d
+++
@@ -1,6 +0,0 @@
-cmd_out/Debug/obj.target/ogg/src/framing.o := cc '-DPIC' '-DHAVE_CONFIG_H' '-DDEBUG' '-D_DEBUG' -Iconfig/mac/x64 -Iinclude  -fasm-blocks -mpascal-strings -Os -gdwarf-2 -arch x86_64  -MMD -MF out/Debug/.deps/out/Debug/obj.target/ogg/src/framing.o.d.raw  -c -o out/Debug/obj.target/ogg/src/framing.o src/framing.c
-out/Debug/obj.target/ogg/src/framing.o: src/framing.c include/ogg/ogg.h \
-  include/ogg/os_types.h
-src/framing.c:
-include/ogg/ogg.h:
-include/ogg/os_types.h:
